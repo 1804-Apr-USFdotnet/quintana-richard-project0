dffc82eb4044c199becf13fcba9f5c2eda0e31b2
Richard <quintanara@hotmail.com>
2018-04-23 08:31:54 -0700

    challenge

---
+++ CodeChallenge/CodeChallenge/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace CodeChallenge
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string a, backwards = "";
+			Console.WriteLine(" Enter string");
+			a = Console.ReadLine();
+
+			// create the string backwards
+			for (int i = a.Length - 1; i >= 0; i--) 
+			{
+				backwards += a[i].ToString();
+			}
+			// check to see if its a palindrome
+			if (backwards == a) 
+			{
+				Console.WriteLine("String is Palindrome \n Entered String Was {0} and backward string is {1}", a, backwards);
+			}
+			else
+			{
+				Console.WriteLine("String is not Palindrome \n Entered String Was {0} and backward string is {1}", a, backwards);
+			}
+			Console.ReadKey();
+		}
+	}
+}
